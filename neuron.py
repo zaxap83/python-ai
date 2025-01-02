a3798ea62aebf72693faf1400cc954c538b0ff46
zax <paqapar@gmail.com>
2025-01-02 19:31:02 +0200

    Som explaining comments for neuron actions

--- neuron.py
+++ neuron.py
@@ -3,12 +3,15 @@ class Neuron:
     lastError = 1 # random value, it must be > correctionStep to start the train
     correctionStep = 0.0001 # smaller correction will give better result but will take more time
 
+    # main action to calculate necessary value
     def processInputData(self, inputValue):
         return inputValue * self.weight
 
+    # additional action for reverse conversion
     def restoreInputData(self, outputValue):
         return outputValue / self.weight
 
+    # train action, in this way the neuron learns to find the correct value
     def train(self, inputValue, expectedResult):
         actualResult = inputValue * self.weight
         self.lastError = expectedResult - actualResult
