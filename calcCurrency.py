c0f183c7578a1992baefcf42d35884f5ef16ea58
zax <paqapar@gmail.com>
2025-01-02 21:13:48 +0200

    Script execution spent time

--- calcCurrency.py
+++ calcCurrency.py
@@ -1,14 +1,18 @@
 import neuron
+from datetime import datetime
 
 usd = 1
 uah = 42.26
+checkValue = 100
 
 neuron = neuron.Neuron()
 
 i = 0
+startTime = datetime.now()
 while neuron.lastError > neuron.correctionStep or neuron.lastError < -neuron.correctionStep:
     i = i + 1
     neuron.train(usd, uah)
     print('Iteration', i, ' inaccuracy ', neuron.lastError)
 
-print('Result: ', neuron.processInputData(100))
+print('Result: $', checkValue, ' = ', round(neuron.processInputData(checkValue), 2), 'UAH')
+print('Time spent: ', datetime.now() - startTime)
